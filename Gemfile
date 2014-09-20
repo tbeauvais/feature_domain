be4be0cdd76792e6833fd74ef3b52ed7a389f2f1
tbeauvais <tbeauvais1@gmail.com>
2014-09-20 12:44:17 -0400

    Initial checkin of feature based sample domain

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'thin'
+gem 'slim'
+
+gem 'sinatra-assetpack'
+
+gem 'coffee-script'
+
+group :test do
+  gem 'rspec'
+end
+
+group :development, :test do
+  gem 'jasmine'
+  gem 'pry'
+end
---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'slim'
+require 'coffee-script'
+require 'pry'
+
+class App < Sinatra::Base
+
+  set :root, File.dirname(__FILE__) # You must set app root
+
+  register Sinatra::AssetPack
+
+  assets do
+    serve '/js',     :from => 'app/js'
+    serve '/css',    :from => 'app/css'
+    serve '/images', :from => 'app/images'
+
+    js :app, [
+        '/js/vendor/**/*.js',
+        '/js/assets/**/*.js',
+        '/js/*.js'
+    ]
+
+    css :app, [
+        '/css/*.css'
+    ]
+
+  end
+
+  get '/' do
+    slim :index
+  end
+
+end
+
+
+
+
